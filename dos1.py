8600af84436237b444737837fe9fba5313809933
cavdar59 <41127292+cavdar59@users.noreply.github.com>
2019-07-12 12:29:52 +0300

    Update dos1.py

--- dos1.py
+++ dos1.py
@@ -4,13 +4,8 @@ import time
 import socket
 import random
 #Code Time
-from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
+
+
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
